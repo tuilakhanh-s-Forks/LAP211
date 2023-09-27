8c51d80961de4c7ccd5cedf53adad818e33add2a
Khánh Lê Hoàng <bacdau852@gmail.com>
2023-09-27 10:44:57 +0700

    Refactor Validation

--- src/utils/Validation.java
+++ src/utils/Validation.java
@@ -21,63 +21,50 @@ public class Validation implements IValidation{
         // vong lap su dung de nguoi dung nhap den khi dung 
         while (true) {
             System.out.println(msg);
-            // allow user input a string 
-            String input_raw = sc.next().trim();
-            if(status.equals(Status.UPDATE) && input_raw.isBlank()){
-                return input_raw;
+            String input = sc.next().trim();
+            
+            if(status.equals(Status.UPDATE) && input.isBlank()){
+                return input;
             }
             // input == null or do dai = 0 => rong 
-            if (input_raw == null || input_raw.length() == 0) {
-                // error
+            if (input == null || input.length() == 0) {
                 System.err.println("Must input a string not empty !!!");
                 System.out.println("Please enter again!");
-                continue;
+            } else {
+                return input;
             }
-            return input_raw;
         }
     }
 
     @Override
     public String checkProductCodeExist(String msg, List<Product> listProduct, Status status) {
         while (true) {
-            // khai bao bien co de kiem tra xem co trung hay khong, neu trung thi flag = 1 
             int flag = 0;
             // NHAP ID DE CHECK 
-            String id = checkString(msg,status);
+            String id = checkString(msg, status);
             
-            for (Product item : listProduct) {
-                if (item.getCode().equals(id)) {
-                    System.err.println("Id existed!!Please enter again");
-                    flag = 1;
-                    break;
-                }
+            boolean isDuplicate = listProduct.stream().anyMatch(item -> item.getCode().equals(id));
+            
+            if (isDuplicate) {
+                System.err.println("ID already exists! Please enter again.");
+            } else {
+                return id;
             }
-            if (flag == 1) {
-                continue;
-            }
-            return id;
         }
     }
 
     @Override
     public String checkReceiptCodeExist(String msg, List<WareHouse> listWareHouse) {
         while (true) {
-            // khai bao bien co de kiem tra xem co trung hay khong, neu trung thi flag = 1 
-            int flag = 0;
-            // NHAP ID DE CHECK 
             String id = checkString(msg,Status.NONE);
             
-            for (WareHouse item : listWareHouse) {
-                if (item.getCode().equals(id)) {
-                    System.err.println("Receipt code existed!!Please enter again");
-                    flag = 1;
-                    break;
-                }
+            boolean isDuplicate = listWareHouse.stream().anyMatch(item -> item.getCode().equals(id));
+            
+            if (isDuplicate) {
+                System.err.println("Receipt code already exists! Please enter again.");
+            } else {
+                return id;
             }
-            if (flag == 1) {
-                continue;
-            }
-            return id;
         }
     }
 
@@ -90,17 +77,16 @@ public class Validation implements IValidation{
             String dateStr = checkString(msg,status);
             try {
                 sdf.parse(dateStr);
+                return dateStr;
             } catch (ParseException e) {
                 System.err.println("Incorrect date must input by format MM/dd/yyyy ! Please enter again !");
-                continue;
             }
-            return dateStr;
         }
 
     }
 
     @Override
-    public String checkAfterDate(String msg, String pd, Status status) {
+    public String checkAfterDate(String msg, String productionDate, Status status) {
         String dateFormat = "MM/dd/yyyy";
         DateFormat sdf = new SimpleDateFormat(dateFormat);
         sdf.setLenient(false);
@@ -108,15 +94,14 @@ public class Validation implements IValidation{
             String initDate = checkBeforeDate(msg,status);
             try {
                 Date d1 = sdf.parse(initDate);
-                String productionDate = pd;
                 Date d2 = sdf.parse(productionDate);
+                    
                 if (d1.compareTo(d2) < 0) {
-                    System.out.println("Expiration date must large than production date ! Please enter again !");
-                    continue;
+                    System.out.println("Expiration date must be after production date. Please enter again.");
+                } else {
+                    return initDate;
                 }
-                return initDate;
             } catch (ParseException ex) {
-                
                 continue;
             }
 
@@ -128,13 +113,11 @@ public class Validation implements IValidation{
         while(true){
             String type = checkString(msg,status);
             
-            if(!(type.equals("Daily") || type.equals("Long")) ){
-                System.err.println("Must input 1 in 2 type product is 'Daily' or 'Long' ! Please input again !");
-                continue;
+            if (!type.equalsIgnoreCase("Daily") && !type.equalsIgnoreCase("Long")) {
+                System.err.println("Please enter either 'Daily' or 'Long'. Please input again.");
+            } else {
+                return type;
             }
-            
-            return type;
-            
         }
     }
 
@@ -145,43 +128,40 @@ public class Validation implements IValidation{
             
             if(!((type.equals("Small")) || (type.equals("Medium")) || (type.equals("Large")))){
                 System.err.println("Must input 1 in 3 size product is 'Small' or 'Medium' or 'Large' ! Please input again !");
-                continue;
+            } else {
+                return type;
             }
-            
-            return type;
-            
         }
     }
 
     @Override
     public int checkInt(String msg, int min, int max, Status status) {
-    
-// vong lap su dung de nguoi dung nhap den khi dung 
+    // vong lap su dung de nguoi dung nhap den khi dung 
         while (true) {
-            
             // allow user input a string 
             String input_raw = checkString(msg,status);
+            
             if(input_raw.isBlank() && status.equals(Status.UPDATE)){
                 return -1;
             }
+            
             try {
                 // loi nhap sai dinh dang so 
                 int input = Integer.parseInt(input_raw);
                 // loi nhap ngoai range cho phep
-                if (input < min || input > max) {
-                    System.err.println("Must input a number from " + min + "to " + max);
-                    continue;
+                if (isWithinRange(input, min, max)) {
+                    return input;
+                } else {
+                    System.err.println("Input must be a number between " + min + " and " + max + ".");
                 }
-                return input;
             } catch (NumberFormatException e) {
-
-                System.err.println("Must enter a number");
+                System.err.println("Must enter a valid number");
                 continue;
             }
 
         }
     }
-
+ 
     @Override
     public double checkDouble(String msg, double min, double max, Status status) {
           // vong lap su dung de nguoi dung nhap den khi dung 
@@ -196,45 +176,54 @@ public class Validation implements IValidation{
                 // loi nhap sai dinh dang so 
                 double input = Double.parseDouble(input_raw);
                 // loi nhap ngoai range cho phep
-                if (input < min || input > max) {
-                    System.err.println("Must input a number from " + min + "to " + max);
-                    continue;
+                if (isWithinRange(input, min, max)) {
+                    return input;
+                } else {
+                    System.err.println("Input must be a number between " + min + " and " + max + ".");
                 }
-                return input;
             } catch (NumberFormatException e) {
 
-                System.err.println("Must enter a number");
+                System.err.println("Must enter a valid number");
                 continue;
             }
 
         }
     }
+    
+     private <T extends Number> boolean isWithinRange(T value, T min, T max) {
+        double doubleValue = value.doubleValue();
+        double doubleMin = min.doubleValue();
+        double doubleMax = max.doubleValue();
+
+        return doubleValue >= doubleMin && doubleValue <= doubleMax;
+    }
 
     @Override
     public boolean checkYesOrNo(String msg) {
-while (true) {
+        while (true) {
             String input = checkString(msg,Status.NONE);
+            
             if (input.equalsIgnoreCase("Y")) {
                 return true;
             } else if (input.equalsIgnoreCase("N")) {
                 return false;
             } else {
                 System.err.println("Must input Y or N to select option");
-                continue;
             }
-        }    }
+        }    
+    }
 
     @Override
     public boolean checkUpdateOrDelete(String msg) {
         while (true) {
             String input = checkString(msg,Status.NONE);
+            
             if (input.equalsIgnoreCase("U")) {
                 return true;
             } else if (input.equalsIgnoreCase("D")) {
                 return false;
             } else {
                 System.err.println("Must input U or D to select option");
-                continue;
             }
         }
     }
@@ -249,7 +238,6 @@ while (true) {
                 return false;
             } else {
                 System.err.println("Must input F or C to select option");
-                continue;
             }
         }
     }
@@ -263,14 +251,9 @@ while (true) {
                 return false;
             } else {
                 System.err.println("Must input I or E to select option");
-                continue;
             }
         }
     }
-    
-
-    
-    
 }
 
 
