e16a2d2ccb1ae2d122270793bb016e00ea251af5
Khánh Lê Hoàng <bacdau852@gmail.com>
2023-09-27 11:38:50 +0700

    Stuff

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package manage;
+package controller;
 
 import entities.DailyProduct;
 import entities.LongProduct;
 import entities.Product;
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +15,22 @@ import utils.FileManage;
  *
  * @author bravee06
  */
-public class ProductManage implements IProductManage{
+public class ProductController implements IProductController{
     
-    public List<Product> listProduct;
-    public FileManage fileManage;
-    public ProductManage(){
+    private List<Product> listProduct;
+    private FileManage fileManage;
+    
+    public ProductController(){
         listProduct = new ArrayList<>();
         fileManage = new FileManage();
 
     }
+    
+    @Override
+    public List<Product> getListProduct() {
+        return listProduct;
+    }
+    
     @Override
     public void addProduct(Product p) {
         listProduct.add(p);
@@ -41,94 +41,90 @@ public class ProductManage implements IProductManage{
         String code = newProduct.getCode();
         String type = newProduct.getType();
         String name = newProduct.getName();
-        int quanti = newProduct.getQuantity();
-        String codeOld = oldProduct.getCode();
-        String typeOld = oldProduct.getType();
-        String nameOld = oldProduct.getName();
-        int quantiOld = oldProduct.getQuantity();
+        int quantity = newProduct.getQuantity();
         
-        if(code.isBlank()){
-            newProduct.setCode(codeOld);
+        if (code.isBlank()) {
+            newProduct.setCode(oldProduct.getCode());
         }
-        if(name.isBlank()){
-            newProduct.setName(nameOld);
+        if (name.isBlank()) {
+            newProduct.setName(oldProduct.getName());
         }
-        if(quanti == -1){
-            newProduct.setQuantity(quantiOld);
+        if (quantity < 0) {
+            newProduct.setQuantity(oldProduct.getQuantity());
         }
-        if(type.isBlank()){
-            newProduct.setType(typeOld);
-        }
+        
         // 1 trong 2 dang là 'Long' or 'Daily'
-        if(newProduct.getType().equals("Long") && oldProduct.getType().equals("Long") ){
-            LongProduct lp = (LongProduct)newProduct;
-            LongProduct lpOld = (LongProduct)oldProduct;
-            if(lp.getManufacturingDate().isBlank())
-            lp.setManufacturingDate(lpOld.getManufacturingDate());
-            if(lp.getExpirationDate().isBlank())
-            lp.setExpirationDate(lpOld.getExpirationDate());
+        if (oldProduct instanceof LongProduct && newProduct instanceof LongProduct) {
+            LongProduct newLongProduct = (LongProduct) newProduct;
+            LongProduct oldLongProduct = (LongProduct) oldProduct;
+            if(newLongProduct.getManufacturingDate().isBlank()) {
+                newLongProduct.setManufacturingDate(oldLongProduct.getManufacturingDate());
+            }
+            if(newLongProduct.getExpirationDate().isBlank()) {
+                newLongProduct.setExpirationDate(oldLongProduct.getExpirationDate());
+            }
         }
-        if(newProduct.getType().equals("Daily") && oldProduct.getType().equals("Daily")){
-            
-                DailyProduct lp = (DailyProduct)newProduct;
-                DailyProduct lpOld = (DailyProduct)oldProduct;
-                if(lp.getSize().isBlank())
-                lp.setSize(lpOld.getSize());
-            if(lp.getUnit() == -1d)
-            lp.setUnit(lpOld.getUnit());
-            
-            
+        if(oldProduct instanceof DailyProduct && newProduct instanceof DailyProduct){
+            DailyProduct newDailyProduct = (DailyProduct) newProduct;
+            DailyProduct oldDailyProduct = (DailyProduct) oldProduct;
+            if(newDailyProduct.getSize().isBlank()) {
+                newDailyProduct.setSize(oldDailyProduct.getSize());
+            }
+            if(newDailyProduct.getUnit() == -1d) {
+                newDailyProduct.setUnit(oldDailyProduct.getUnit()); 
+            }
         }
-        
         return newProduct;
     }
 
     @Override
-    public void deleteProduct(Product p) {
-        listProduct.remove(p);
+    public boolean deleteProduct(Product productToDelete) {
+        return listProduct.remove(productToDelete);
+    }
+    
+    public void show(List<Product> list){
+        for(Product p: list){
+            System.out.println(p);
+        }
     }
 
     @Override
     public void showAllProduct(boolean option) {
-        
         // true: file , false: collection 
-        if(option){
+        if (option) {
             try {
                 showByFile("product.dat");
             } catch (IOException ex) {
-                Logger.getLogger(ProductManage.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }else{
+        } else {
             showByCollection();
         }
     }
-    public void show(List<Product> list){
-        for(Product p: list){
-                System.out.println(p);
-            }
-    }
+    
     public void showByCollection(){
         show(listProduct);
     }
+    
     public void showByFile(String pathFile) throws FileNotFoundException, IOException{
         // P0001,ABC,2,Long,20/12/2003,10/10/2005 
         // P0002,ABC,2,Daily,3.5,Small 
         List<Product> listProductInFile = new ArrayList<>();
         List<String> listData = fileManage.loadFromFile(pathFile);
-         for (String line : listData) {
-            Product newProduct = null;
+        for (String line : listData) {
             String[] data = line.split(",");
-           
-           
             String code = data[0];
             String name = data[1];
             int quanti = Integer.parseInt(data[2].substring(1));
             String type = data[3].substring(1);
-            if(type.equals("Long")){
+            
+            Product newProduct;
+            
+            if (type.equals("Long")) {
                 String pDate = data[4];
                 String eDate = data[5];
                 String sup = data[6];
-                newProduct = new LongProduct(pDate,eDate,sup,code,name,quanti,type);
+                newProduct = new LongProduct(pDate,eDate,sup,code,name,quanti,type) {};
             }else{
                 String size = data[5];
                 double unit = Double.parseDouble(data[4]);
@@ -136,45 +132,40 @@ public class ProductManage implements IProductManage{
             }
             listProductInFile.add(newProduct);
         }
-         show(listProductInFile);
+        show(listProductInFile);
     }
     
     public Product getProductByCode(String code){
-        for(Product p : listProduct ){
-            if(p.getCode().equals(code)){
-                return p;
-            }
-        }
-        return null;
+        return listProduct.stream()
+            .filter(p -> p.getCode().equals(code))
+            .findFirst()
+            .orElse(null);
     }
     
     
     @Override
     public void loadData(List<String> dataFile) {
         for (String line : dataFile) {
-            Product newProduct = null;
-            String[] data = line.split(",");
-           
-           
+            String[] data = line.split(",");       
             String code = data[0];
             String name = data[1];
             int quanti = Integer.parseInt(data[2].substring(1));
             String type = data[3].substring(1);
+            
+            Product newProduct;
+            
             if(type.equals("Long")){
                 String pDate = data[4];
                 String eDate = data[5];
                 String sup = data[6];
-                newProduct = new LongProduct(pDate,eDate,sup,code,name,quanti,type);
+                newProduct = new LongProduct(pDate,eDate,sup,code,name,quanti,type) {};
             }else{
                 String size = data[5];
                 double unit = Double.parseDouble(data[4]);
                 newProduct = new DailyProduct(size,unit,code,name,quanti,type);
             }
+            
             listProduct.add(newProduct);
         }
     }
-       
-       
-        
-    
 }
